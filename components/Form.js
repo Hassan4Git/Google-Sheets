0ce2fa7ccd2b8f69f229e5757539cd8e38fd112e
Hassan Ahmed <4hahmed@gmail.com>
2023-01-16 11:40:52 -0500

    Finished basic form with MUI

--- components/Form.js
+++ components/Form.js
@@ -1,16 +1,36 @@
 import styles from '../styles/Form.module.scss'
 import Image from 'next/image'
+import { TextField, Select, MenuItem, InputLabel, FormControl } from '@mui/material'
+import { useState } from 'react'
 
 const Form = () => {
+    const [packageNum, setPackageNum] = useState('')
+    const handleChange = (event) => {
+        setPackageNum(event.target.value)
+    }
     return (
         <div className={styles.container}>
-            <Image src='/google_sheets_logo.png' height={70} width={50} alt='Google Sheets' id='top' />
-            <Image src='/google_sheets_logo.png' height={70} width={50} alt='Google Sheets' id='bottom' />
+            <Image src='/google_sheets_logo.png' height={70} width={50} alt='Google Sheets' id={styles.bottom} />
+            <Image src='/google_sheets_logo.png' height={70} width={50} alt='Google Sheets' id={styles.top} />
             <form className={styles.form}>
                 <h1>Order</h1>
-                <input type='text' placeholder='Lorem' />
-                <input type='text' placeholder='Lorem' />
-                <input type='text' placeholder='Lorem' />
+                <TextField variant='outlined' label='Name' fullWidth />
+                <TextField variant='outlined' label='Phone Number' fullWidth />
+                <FormControl style={{width: "100%"}}>
+                    <InputLabel id="packagePicker">Package</InputLabel>
+                    <Select
+                        labelId="packagePicker"
+                        id="packageSelect"
+                        value={packageNum}
+                        label="Package"
+                        onChange={handleChange}
+                        fullWidth
+                        >
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                    </Select>                
+                </FormControl>
                 <input type='submit' value='Submit Order' />
             </form>
         </div>
